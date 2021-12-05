93e558cf78dca3ac8716a073e35bfcad7630b587
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-12-05 17:12:39 +0100

    add first meeting

---
+++ R/2020-01-31.R
@@ -0,0 +1,10 @@
+social(
+  input = here::here("assets/poster.Rmd"),
+  output = here::here("meetups/2020-01-31"),
+  rmd_params = list(
+    title = 'Réunion d\'Information "R User Group"',
+    author = "Mathilde Boissel, Julien Hamonier & Mickaël Canouil",
+    date = "Vendredi 31 Janvier 2020 - 12:00 (CET / UTC + 2)",
+    website = "[rlille.fr](https://rlille.fr/)"
+  )
+)
