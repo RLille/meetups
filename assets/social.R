a7126f3d3dd58522f1cbede6e87e89f6452d5c14
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-02-06 16:11:49 +0100

    fix(README/social): new arg blogpost

--- assets/social.R
+++ assets/social.R
@@ -31,10 +31,9 @@ social <- function(
   input_blogpost = here::here("assets/blogpost.Rmd"),
   output,
   rmd_params,
-  chrome_path,
   delay = 1
 ) {
-  render_meetup <- function(input_poster, input_announcement, input_blogpost, output, rmd_params, chrome_path, delay = 1) {
+  render_meetup <- function(input_poster, input_announcement, input_blogpost, output, rmd_params, delay = 1) {
     devnull <- sapply(
       X = file.path(output, c("materials", "ads")),
       FUN = dir.create, showWarnings = FALSE, recursive = TRUE
@@ -42,7 +41,13 @@ social <- function(
     # file.create(file.path(output, c("materials", "ads"), ".gitkeep"))
 
     readme_file <- file.path(output, "materials", "README.md")
-    if (!file.exists(readme_file) | file.mtime(readme_file) < file.mtime("assets/social.R")) {
+    if (
+      !file.exists(readme_file) |
+      (
+        file.mtime(readme_file) < file.mtime("assets/social.R") &
+        as.Date(basename(output)) < Sys.Date()
+      )
+    ) {
       writeLines(
         sprintf(
           "# %s\n\nSpeaker: %s\n\n- Video: \n\n- Slides: ",
@@ -53,27 +58,37 @@ social <- function(
     }
 
     if (all(nzchar(rmd_params[c("meetup_id", "abstract", "biography")]))) {
+      output_announcement <- sprintf("%s/ads/%s-email.md", output, basename(output))
+      if (file.exists(output_announcement)) unlink(output_announcement)
       rmarkdown::render(
         input = input_announcement,
-        output_file = sprintf("%s/ads/%s-email.md", output, basename(output)),
+        output_file = output_announcement,
         encoding = "UTF-8",
-        params = rmd_params[c(
-          "title", "author", "date", "date_short",
-          "meetup_id", "abstract", "biography",
-          "language"
+        params = rmd_params[intersect(
+          c(
+            "title", "author", "date", "date_short",
+            "meetup_id", "abstract", "biography",
+            "language"
+          ),
+          names(rmd_params)
         )]
       )
     }
 
     if (all(nzchar(rmd_params[c("meetup_id", "abstract", "biography")]))) {
+      output_blogpost <- sprintf("%s/ads/%s-blogpost.md", output, basename(output))
+      if (file.exists(output_blogpost)) unlink(output_blogpost)
       rmarkdown::render(
         input = input_blogpost,
-        output_file = sprintf("%s/ads/%s-blog.md", output, basename(output)),
+        output_file = output_blogpost,
         encoding = "UTF-8",
-        params = rmd_params[c(
-          "title", "author", "date", "date_short",
-          "meetup_id", "abstract", "biography",
-          "language"
+        params = rmd_params[intersect(
+          c(
+            "title", "author", "date", "date_short",
+            "meetup_id", "abstract", "biography",
+            "language"
+          ),
+          names(rmd_params)
         )]
       )
     }
@@ -82,17 +97,20 @@ social <- function(
       input = input_poster,
       output_dir = tempdir(),
       encoding = "UTF-8",
-      params = rmd_params[c(
-        "title", "subtitle", "author",
-        "institute", "date",
-        "picture", "website"
+      params = rmd_params[intersect(
+        c(
+          "title", "subtitle", "author",
+          "institute", "date",
+          "picture", "website"
+        ),
+        names(rmd_params)
       )]
     )
     on.exit(unlink(xaringan_poster))
     output_pdf <- sprintf("%s/ads/%s.pdf", output, basename(output))
     output_png <- sprintf("%s/ads/%s.png", output, basename(output))
     xaringanBuilder::build_pdf(xaringan_poster, output_file = output_pdf)
-    xaringanBuilder::build_png(xaringan_poster, output_file = output_png)
+    xaringanBuilder::build_png(xaringan_poster, output_file = output_png, density = 300)
 
     invisible(output_pdf)
   }
@@ -101,6 +119,7 @@ social <- function(
     args = list(
       input_poster = input_poster,
       input_announcement = input_announcement,
+      input_blogpost = input_blogpost,
       output = output,
       rmd_params = rmd_params,
       delay = delay
