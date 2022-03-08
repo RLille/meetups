1056b167d41fb26925aaba8ae68c3cfb49c45848
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-03-08 21:46:29 +0100

    style: tweak a bit abstract

--- R/2022-04-07.R
+++ R/2022-04-07.R
@@ -8,8 +8,8 @@ create_meetup(
     date_short = "2022-04-07", # date of the meetup using YYYY-MM-DD format
     picture = "https://github.com/privefl.png", # URL of the speaker's picture, or "none"
     website = "https://privefl.github.io/", # URL of the speaker's website
-    abstract = "R package {bigstatsr} (https://github.com/privefl/bigstatsr) provides functions for fast statistical analysis of large-scale data encoded as matrices. The package can handle matrices that are too large to fit in memory. Package bigstatsr is based on a similar format (called `FBM`) as the format `big.matrix` provided by the R package {bigmemory}.
-Package {bigstatsr} enables users with a laptop to perform statistical analysis of several dozens of gigabytes of data, and HPC users to handle matrices of hundreds of GB. The package is fast and efficient because of four different reasons. First, {bigstatsr} is memory-efficient
+    abstract = "R package {bigstatsr} (https://github.com/privefl/bigstatsr) provides functions for fast statistical analysis of large-scale data encoded as matrices. The package can handle matrices that are too large to fit in memory. The Package {bigstatsr} is based on a similar format (called `FBM`) as the format `big.matrix` provided by the R package {bigmemory}.
+The Package {bigstatsr} enables users with a laptop to perform statistical analysis of several dozens of gigabytes of data, and HPC users to handle matrices of hundreds of GB. The package is fast and efficient because of four different reasons. First, {bigstatsr} is memory-efficient
 because it uses only small chunks of data at a time. Second, special care has been taken to implement effective algorithms. Third, `FBM` objects use memory-mapping, which provides efficient accesses to matrices. Finally, as matrices are stored on-disk, many processes can easily access them in parallel.
 
 The main features currently available in {bigstatsr} are:
