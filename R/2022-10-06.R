f93f4123637e4c9df6c32dc17bef898b8bbfec8a
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-09-24 14:51:42 +0200

    fix: typos

--- R/2022-10-06.R
+++ R/2022-10-06.R
@@ -10,13 +10,13 @@ create_meetup(
     website = "https://rlille.fr/", # URL of the speaker's website
     abstract = paste(
       "R Lille a vu le jour en février 2020, malgré le contexte, c'est tout de même 11 rencontres en ligne ou en présentiel.",
-      "Je vous propose ici de faire le point sur ces presques deux ans de R Lille, sur vos vos attentes, vos envies, mais aussi de se retrouver pour tout simplement discuter et se rencontrer.",
+      "Je vous propose ici de faire le point sur ces presques deux ans de R Lille, sur vos attentes, vos envies, mais aussi de se retrouver pour tout simplement discuter et se rencontrer.",
       "Rejoignez moi pour un afterwork le Jeudi 6 Octobre 2022 à 18:30 CET dans un bar de Lille.",
       "À des fins d'organisation, je vous invite à vous inscrire pour qu'une réservation dans un établissement puisse être faite.",
       "À très bientôt !",
       sep = "  \n"
     ),
-    biography = " ", # biography of the speaker using Markdown syntax
+    biography = "R Lille", # biography of the speaker using Markdown syntax
     language = "fr", # language of the talk
     meetup_id = "288690120" # to be filled by R Lille organisers
   )
